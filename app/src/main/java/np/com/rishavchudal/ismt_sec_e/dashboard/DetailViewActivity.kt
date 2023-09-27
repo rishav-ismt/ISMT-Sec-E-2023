6b4784ece92ceedae56292f83cd39b4a9aadf82e
Personal <personal@Rishav-Chudal-Esewa-MacBook-Pro.local>
2023-09-27 08:34:04 +0545

    Added for send sms

--- app/src/main/java/np/com/rishavchudal/ismt_sec_e/dashboard/DetailViewActivity.kt
+++ app/src/main/java/np/com/rishavchudal/ismt_sec_e/dashboard/DetailViewActivity.kt
@@ -1,14 +1,24 @@
 package np.com.rishavchudal.ismt_sec_e.dashboard
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.telephony.SmsManager
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import np.com.rishavchudal.ismt_sec_e.AppConstants
 import np.com.rishavchudal.ismt_sec_e.BitmapScalar
 import np.com.rishavchudal.ismt_sec_e.R
@@ -17,7 +27,7 @@ import np.com.rishavchudal.ismt_sec_e.database.Product
 import np.com.rishavchudal.ismt_sec_e.database.TestDatabase
 import np.com.rishavchudal.ismt_sec_e.databinding.ActivityDetailViewBinding
 import java.io.IOException
-import java.lang.Exception
+
 
 class DetailViewActivity : AppCompatActivity() {
     private lateinit var detailViewBinding: ActivityDetailViewBinding
@@ -37,6 +47,7 @@ class DetailViewActivity : AppCompatActivity() {
 
     companion object {
         const val RESULT_CODE_REFRESH = 2001
+        const val SMS_PERMISSIONS_REQUEST_CODE = 111
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,6 +61,20 @@ class DetailViewActivity : AppCompatActivity() {
         //Populate Data to the views
         populateDataToTheViews(product)
         setUpButtons()
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == SMS_PERMISSIONS_REQUEST_CODE && areSmsPermissionsGranted(this)) {
+            showSmsBottomSheetDialog()
+        } else {
+            UiUtility.showToast(this, "Please provide permission for SMS")
+        }
     }
 
     private fun populateDataToTheViews(product: Product?) {
@@ -125,6 +150,16 @@ class DetailViewActivity : AppCompatActivity() {
 
     private fun setUpShareButton() {
         //TODO
+        detailViewBinding.ibShare.setOnClickListener {
+            if (areSmsPermissionsGranted(this)) {
+                showSmsBottomSheetDialog()
+            } else {
+                requestPermissions(
+                    smsPermissionsList()!!.toTypedArray(),
+                    SMS_PERMISSIONS_REQUEST_CODE
+                )
+            }
+        }
     }
 
     private fun deleteProduct() {
@@ -153,6 +188,77 @@ class DetailViewActivity : AppCompatActivity() {
                 }
             }
         }.start()
+    }
+
+    private fun areSmsPermissionsGranted(context: Context): Boolean {
+        var areAllPermissionGranted = false
+        for (permission in smsPermissionsList()!!) {
+            if (ContextCompat.checkSelfPermission(context, permission)
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                areAllPermissionGranted = true
+            } else {
+                areAllPermissionGranted = false
+                break
+            }
+        }
+        return areAllPermissionGranted
+    }
+    private fun smsPermissionsList(): List<String>? {
+        val smsPermissions: MutableList<String> = ArrayList()
+        smsPermissions.add(Manifest.permission.READ_SMS)
+        smsPermissions.add(Manifest.permission.SEND_SMS)
+        return smsPermissions
+    }
+
+    private fun showSmsBottomSheetDialog() {
+        //TODO
+        val smsBottomSheetDialog = BottomSheetDialog(this)
+        smsBottomSheetDialog.setContentView(R.layout.bottom_sheet_dialog_send_sms)
+
+        val tilContact: TextInputLayout? = smsBottomSheetDialog.findViewById(R.id.til_contact)
+        val tieContact: TextInputEditText? = smsBottomSheetDialog.findViewById(R.id.tie_contact)
+        val sendSmsButton: MaterialButton? = smsBottomSheetDialog.findViewById(R.id.mb_send_sms)
+
+        tilContact?.setEndIconOnClickListener {
+            //TODO open Contact Activity
+        }
+
+        sendSmsButton?.setOnClickListener {
+            val contact = tieContact?.text.toString()
+            //TODO validation
+            if (contact.isBlank()) {
+                tilContact?.error = "Enter Contact"
+            } else {
+                sendSms(contact)
+            }
+        }
+    }
+
+    private fun sendSms(contact: String) {
+        val smsManager: SmsManager = SmsManager.getDefault()
+        val message = """
+            Item: ${product!!.title}
+            Price: ${product!!.price}
+            Description: ${product!!.description.substring(50)}
+            """.trimIndent()
+        smsManager.sendTextMessage(
+            contact,
+            null,
+            message,
+            null,
+            null
+        )
+
+        //If the above SMS manager didn't work
+        openSmsAppToSendMessage(contact, message)
+    }
+
+    private fun openSmsAppToSendMessage(contact: String, message: String) {
+        val sendIntent = Intent(Intent.ACTION_VIEW)
+        sendIntent.data = Uri.parse("smsto:$contact")
+        sendIntent.putExtra("sms_body", message)
+        startActivity(intent)
     }
 
     private fun setResultWithFinish(resultCode: Int) {
